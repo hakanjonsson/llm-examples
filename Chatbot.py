9c9c73599359a09c410d55ebf9e15e8124db2bb3
Håkan Jonsson <hajons@gmail.com>
2023-08-04 14:30:28 +0000

    initial prompt

--- Chatbot.py
+++ Chatbot.py
@@ -17,7 +17,7 @@ st.text("I am a chatbot and will help you with your German language practice.")
 st.text("I will ask you questions and will correct your answers. Lets start!")
 if "messages" not in st.session_state:
  #   initial_prompt = "Please act as my German teacher. You ask me questions in German at level B2. You then wait for my response. Do not answer your own question. You then correct my answer, including providing a grammatical explanation of the correction. After correcting, you ask me a new question in German and await my answer."
-    st.session_state["messages"] = [{"role": "system", "content":"Please act as my German teacher. You ask me one question in German. You then wait for my response. You do not answer your own question. When you have received an answer from me to your question, you provide a corrected answer and a grammatical explanation of it (if incorrect). You then ask me a new question in German, and again wait for my answer."}]
+    st.session_state["messages"] = [{"role": "system", "content":"Please act as my German teacher. You ask me one question in German. You then wait for my response. You do not answer your own question. When you have received an answer from me to your question, you provide a corrected answer and a grammatical explanation of it. You always end by asking me a new question in German."}]
  #   st.session_state.messages.append({"role": "system", "content": initial_prompt})
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
     msg = response.choices[0].message
